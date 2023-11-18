a9bb004bebc7e144ab97224a988a7760782f7ce5
Mathias CHOQUET <mathias.choquet@gmail.com>
2023-11-18 17:37:51 +0100

    improve header

--- SIGMA_Web/src/components/SigmaHeader.vue
+++ SIGMA_Web/src/components/SigmaHeader.vue
@@ -3,38 +3,74 @@ import jsonData from '@/assets/sigma_header_texts.json';
 </script>
 
 <template>
-  <div class="greetings">
-    <img src="@/assets/logo_sigma.png" alt="EPITA Logo" class="logo">
-    <h1>{{ jsonData.title }}</h1>
-    <h3>
-      <a href="#oldStudentsMap">anciens élèves</a>
+  <div>
+    <div class="titre">
+      <div class="logo">
+        <img src="@/assets/logo_sigma.png" alt="SIGMA Logo" class="logo">
+      </div>
+      <div class="texte">
+        <h1>{{ jsonData.title }}</h1>
+      </div>
+    </div>
+    <div class="menu">
+      <a href="#oldStudentsMap">Anciens élèves</a>
       <a href="#restaurantsMap">Les restaurants</a>
       <a href="#composant3">composant3</a>
-      <a href="#composant4">composant4</a>
+      <a href="#composant4">compt4</a>
       <a href="#composant5">composant5</a>
-    </h3>
+    </div>
   </div>
 </template>
 
 <style scoped>
 
-.logo{
-  position: relative;
-  text-align: center;
-  width: 100%;
-}
+.titre {
+    display: flex;
+    align-items: center;
+    @media screen and (max-width: 600px) {
+      flex-direction: column;
+    }
+  }
 
-h3 {
-  font-size: 1.2rem;
-}
+  .logo {
+    flex: 1;
+  }
 
-a{
-  margin-right: 10px;
-}
+  .logo img {
+    max-width: 100%;
+    height: auto;
+  }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+  .texte {
+    flex: 2;
+    max-width: 100%;
+  }
+
+  .menu {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px; /* Ajustez la valeur selon vos besoins */
+    margin: auto; /* Pour centrer le menu */
+    flex-direction: row;
+    font-size: 1.2rem;
+
+        /* Ajout d'une media query pour rendre le menu vertical sur des écrans plus petits */
+    @media screen and (max-width: 600px) {
+      flex-direction: column;
+    }
+  }
+
+  .menu a {
+    color: rgb(21, 44, 106);
+    flex: 1;
+    border: 1px solid #ccc;
+    text-align: center;
+    padding: 0 10px; /* Ajustez la valeur selon vos besoins */
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
 
 </style>
